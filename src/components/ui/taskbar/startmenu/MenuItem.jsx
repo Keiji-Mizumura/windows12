6469a15f42292ce04aec8b5640f24a46eb0d471d
Keiji-Mizumura <keijimizumura@icloud.com>
2024-08-28 19:20:39 +0900

    Fixed window size issue

--- src/components/ui/taskbar/startmenu/MenuItem.jsx
+++ src/components/ui/taskbar/startmenu/MenuItem.jsx
@@ -1,8 +1,8 @@
 import styles from './MenuItem.module.css'
 
-function MenuItem({ title, icon, shutdown, hasDropDown }){
+function MenuItem({ title, icon, shutdown, hasDropDown, onClick }){
     return(
-        <div className={styles.item} style={ shutdown ? {borderTop: "2px solid #999"} : {} }>
+        <div className={styles.item} style={ shutdown ? {borderTop: "2px solid #999"} : {} } onClick={onClick}>
             <img src={icon} />
             <p className={styles.text}>
                 {title}
